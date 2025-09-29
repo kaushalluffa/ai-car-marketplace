200212d8cb2a927fc9917dc1190892dec5ac5936
kaushalluffa <kaushalluffa91@gmail.com>
2025-09-29 19:18:46 -0400

    feat: add comment in apiRoutesMap for fetching car details

--- src/lib/apiRoutesMap.ts
+++ src/lib/apiRoutesMap.ts
@@ -1,7 +1,7 @@
 export const apiRoutesMap = {
     v1: {
         cars: {
-            GET: '/api/v1/cars',
+            GET: '/api/v1/cars', // add id in the end for specific car
             POST: '/api/v1/cars',
             filters: '/api/v1/cars/filters',
         }
