1df2d7a63138040b993d1ce3e6cabc0ea49afb2d
kaushalluffa <kaushalluffa91@gmail.com>
2025-10-01 18:49:47 -0400

    feat: update the middleware to handle the redirection based on auth and onboardi

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,30 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+const isOnboardingRoute = createRouteMatcher("/onboarding");
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  const { isAuthenticated, sessionClaims, redirectToSignIn } = await auth();
+
+  if (
+    isAuthenticated &&
+    isOnboardingRoute(req) &&
+    !sessionClaims?.metadata?.onboardingComplete
+  ) {
+    return NextResponse.next();
+  }
+
+  if (isAuthenticated && !sessionClaims?.metadata?.onboardingComplete) {
+    const onboardingUrl = new URL("/onboarding", req.url);
+    return NextResponse.redirect(onboardingUrl);
+  }
+  if (isAuthenticated && sessionClaims?.metadata?.onboardingComplete && isOnboardingRoute(req)) {
+    const homeUrl = new URL("/", req.url);
+    return NextResponse.redirect(homeUrl);
+  }
+  if (!isAuthenticated && !isOnboardingRoute(req)) {
+    return NextResponse.next();
+  }
+});
 
 export const config = {
   matcher: [
