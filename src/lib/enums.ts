a5a021efc1e8d9c347a7e928909dfa175513227b
kaushalluffa <kaushalluffa91@gmail.com>
2025-10-04 17:01:52 -0400

    feat: add enums file

---
+++ src/lib/enums.ts
@@ -0,0 +1,15 @@
+export enum CarSort {
+    NEWEST = "newest",
+    OLDEST = "oldest",
+    PRICE_LOW_TO_HIGH = "priceLowToHigh",
+    PRICE_HIGH_TO_LOW = "priceHighToLow",
+
+}
+
+export enum BookingStatus {
+  PENDING = "PENDING",
+  CONFIRMED = "CONFIRMED",
+  COMPLETED = "COMPLETED",
+  CANCELLED = "CANCELLED",
+  NO_SHOW = "NO_SHOW",
+}
